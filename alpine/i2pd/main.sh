33f7084bfcc6d9bca8bb69353d94b427e0becb82
LinkWu <wjianl@qq.com>
2018-11-04 16:45:58 +0800

    优化i2pd启动命令

--- alpine/i2pd/main.sh
+++ alpine/i2pd/main.sh
@@ -3,4 +3,8 @@ set -e
 
 ln -s $APP_USER_HOME/certificates $APP_EXTERNAL_DIR/certificates
 
-$APP_USER_BIN/i2pd --datadir $APP_EXTERNAL_DIR --conf $APP_EXTERNAL_DIR/i2pd.conf
+if [ -f "$APP_EXTERNAL_DIR/i2pd.conf" ]; then
+    $APP_USER_BIN/i2pd --datadir $APP_EXTERNAL_DIR --service false --conf $APP_EXTERNAL_DIR/i2pd.conf
+else
+    $APP_USER_BIN/i2pd --datadir $APP_EXTERNAL_DIR --service false
+fi
